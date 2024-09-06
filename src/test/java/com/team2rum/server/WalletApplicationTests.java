5af9cea0a38bc914cc5c48cca8426203749c037b
dh1010a <dh1010a@naver.com>
2024-09-06 11:09:33 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/team2rum/server/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2rum.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
