97ec19986ed8f5243614d55d69cc80311ff4b1d0
dh1010a <dh1010a@naver.com>
2024-09-14 15:09:41 +0900

    test: home test

--- src/main/java/server/domain/HomeController.java
+++ src/main/java/server/domain/HomeController.java
@@ -2,22 +2,25 @@ package server.domain;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Hello, World!";
+    @GetMapping("/test")
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok().body("Hello, World!");
     }
 
-    @PostMapping("/")
-    public String homePost() {
-        return "Hello, World!";
+    @PostMapping("/test")
+    public ResponseEntity<String> home2() {
+        return ResponseEntity.ok().body("Hello, World!");
     }
 }
