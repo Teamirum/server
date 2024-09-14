90ac9726e4e752a66ea5b0da5916c01d7609fcc2
dh1010a <dh1010a@naver.com>
2024-09-14 15:01:57 +0900

    feat: 메인 컨트롤러 추가

---
+++ src/main/java/server/domain/HomeController.java
@@ -0,0 +1,23 @@
+package server.domain;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!";
+    }
+
+    @PostMapping("/")
+    public String homePost() {
+        return "Hello, World!";
+    }
+}
